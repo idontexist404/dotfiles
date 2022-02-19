547e3c848dd0a6a79ab990120b6b92f5eb2991a3
Highlander Magno <highlander9200@gmail.com>
2022-02-19 17:52:25 -0300

    initial commit, base application added

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,18 @@
+-- Config in Lua must come before loading the color scheme
+vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
+vim.cmd [[
+try
+  colorscheme tokyonight
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
+
